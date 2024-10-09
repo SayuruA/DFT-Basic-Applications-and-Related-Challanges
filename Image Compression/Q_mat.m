d09af26c935b898f695a4c307baf81dd7509fe92
SayuruA <sayurushenal141@gmail.com>
2024-10-10 00:43:14 +0530

    Functions 'compress' and 'Q_mat' are tested on the matrix in the task sheet, and successful!

--- Image Compression/Q_mat.m
+++ Image Compression/Q_mat.m
@@ -1,18 +1,20 @@
 function Qn = Q_mat(qualityLevel)
-% This function generates the Quantization matrix for the required quality
+% ** This function generates the Quantization matrix for the required quality
 % level
-    Q50 = load("QuntiMtrx.mat").x; % Quantizatioin Matrix of qualityLevel, 50.
-    % compute tau
-    if (qualityLevel >= 50)
-        tau = (100 - qualityLevel)/50;
-    else
-        tau = 50/qualityLevel;
-    end
 
-    disp(tau);
+Q50 = load("QuntiMtrx.mat").x; % Quantizatioin Matrix of qualityLevel, 50.
 
-    Qn_unclipped = tau*Q50;
-    Qn = round(Qn_unclipped);
-    Qn(Qn > 255) = 255;
-    Qn(Qn < 0) = 0;
+% compute the scalling factor tau
+if (qualityLevel >= 50)
+    tau = (100 - qualityLevel)/50;
+else
+    tau = 50/qualityLevel;
+end
+
+% scale, round-off, and clip the values to the range 0 - 255.
+Qn_unclipped = tau*Q50;
+Qn = round(Qn_unclipped);
+Qn(Qn > 255) = 255;
+Qn(Qn < 0) = 0;
+
 end
--- Image Compression/compress.m
+++ Image Compression/compress.m
@@ -5,8 +5,10 @@ function B_tilde = compress(block_8by8, qualityLevel)
 formattedBlock = double(block_8by8);
 B = formattedBlock -128; % Levelled off by subtracting 128
 C = dct2(B);
+
 % Quantization Matrix for the given 'qualityLevel'
 Qn = Q_mat(qualityLevel); 
+
 % Element-wise division by 'Qn', rounding, and then multiply
 % by 'Qn' again and finally, add the 128 back to the inverse transformed matrix.
 S = round(C./Qn);
