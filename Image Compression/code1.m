fc9bd0b0320a3bfbd94c25016bd48a9a448b659d
SayuruA <sayurushenal141@gmail.com>
2024-10-10 02:30:27 +0530

    Code is fully working for any gray image with dimensions divisible by 8.Further testing on new data required.

--- Image Compression/code1.m
+++ Image Compression/code1.m
@@ -1,19 +1,38 @@
 clc;
 clear;
 
+imageName = 
+Barb = load("Selected-Images\Parrots.mat").B;
 
-Barb = load("Selected-Images\barbara.mat").A;
-[M, N] = size(Barb);
+[M, N] = size(Barb); % the number of rows and columns of the image
+qualityLevel = 50;
+
 compressedBarb = zeros([M,N]);
+totalZeros = 0;
+
+for row = 1: 8: M
+    
+    for col = 1: 8: N
+        
+        imageBlock = Barb(row: row+7, col: col+7);
+        [compressedBlock, Zeros] = compress(imageBlock, qualityLevel);
+        compressedBarb(row: row+7, col: col+7) = compressedBlock;
+        totalZeros = totalZeros + Zeros;
+        
+    end
+end
+compressedBarb = uint8(round(compressedBarb));
+zeroRatio = round(totalZeros/ numel(Barb) * 100, 1);
+
+
+figure;
+subplot(1, 2, 1);
+imshow(Barb);
+title("The Original Image")
+
+subplot(1, 2, 2);
+imshow(compressedBarb);
+title(sprintf('The Compressed, quality level = %d, zeros: %.2f%%', qualityLevel, zeroRatio));
 
 
 
-testBlock = Barb(129:136, 161:168);
-formattedBlock = double(testBlock);
-B = formattedBlock -128; % Levelled off by subtracting 128
-C = dct2(B);
-
-
-Qn = Q_mat(80); % Quantization Matrix
-
-S = round(C./Qn);
