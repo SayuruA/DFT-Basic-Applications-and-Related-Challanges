34042a28f80e6d1d477f0905533a81190a9537ff
SayuruA <sayurushenal141@gmail.com>
2024-10-21 13:02:03 +0530

    Added a new image and created a '.mat' file. Added a new code block to plot the change of compression vs. quality.

--- Image Compression/code1.m
+++ Image Compression/code1.m
@@ -1,8 +1,8 @@
 clc;
 clear;
 
-image = "house";
-qualityLevel = 97.5;
+image = "blonde";
+qualityLevel = 90;
 path = "Selected-Images\\";  
 % get the required image matrix
 % matrices of different image files are stored under different variable
@@ -17,13 +17,27 @@ Im = Im.(fieldname{1});
 
 
 figure;
-subplot(1, 2, 1);
+subplot(1, 3, 1);
 imshow(Im);
 title("The Original Image")
 
-subplot(1, 2, 2);
+subplot(1, 3, 2);
 imshow(compressedIm);
 title(sprintf('The Compressed, quality level = %d, zeros: %.2f%%', qualityLevel, zerosPercentage));
+
+% This is to plot a graph between the comrpression and the quality level
+% May take a little more time
+qua_levels = 10:20:90;
+zero_vals = [];
+for qualityLevel = qua_levels
+    [compressedIm, zerosPercentage] = compressImage(Im, qualityLevel);
+    zero_vals = [zero_vals, zerosPercentage];
+end
+subplot(1, 3, 3);
+plot(qua_levels, zero_vals);
+title("Number of zeros vs. Quality level");
+xlabel("Quality Level");
+ylabel("Numebr of Zeros");
 
 
 
