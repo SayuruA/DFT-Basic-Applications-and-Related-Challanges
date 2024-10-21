c661659e94ff72dfcb258246a01b3638e8557b23
SayuruA <sayurushenal141@gmail.com>
2024-10-21 22:10:06 +0530

    All tasks are done! Organizing the Repo further.

--- Image Compression/RunMe.m
+++ Image Compression/RunMe.m
@@ -2,7 +2,7 @@ clc;
 clear;
 
 image = "town";
-qualityLevel = 99;
+qualityLevel = 70;
 path = "Selected-Images\\";  
 % get the required image matrix
 % matrices of different image files are stored under different variable
@@ -19,27 +19,36 @@ psnr = PSNR(compressedIm, Im);
 text = sprintf("Peak Signal to Noise Ration = %0.2f",round(psnr,2));
 disp(text);
 figure;
-subplot(1, 3, 1);
+subplot(1, 2, 1);
 imshow(Im);
 title("The Original Image")
 
-subplot(1, 3, 2);
+subplot(1, 2, 2);
 imshow(compressedIm);
 title(sprintf('The Compressed, quality level = %d, zeros: %.2f%%', qualityLevel, zerosPercentage));
 
-% This is to plot a graph between the comrpression and the quality level
-% May take a little more time
-% qua_levels = 10:20:90;
+% % This is to plot a graph between the comrpression and the quality level
+% % May take a while
+% qua_levels = 10:10:90;
 % zero_vals = [];
+% psnr_vals = [];
 % for qualityLevel = qua_levels
 %     [compressedIm, zerosPercentage] = compressImage(Im, qualityLevel);
 %     zero_vals = [zero_vals, zerosPercentage];
+%     psnr = PSNR(compressedIm, Im);
+%     psnr_vals = [psnr_vals, psnr];
 % end
-% subplot(1, 3, 3);
+% figure;
+% subplot(1, 2, 1);
 % plot(qua_levels, zero_vals);
 % title("Number of zeros vs. Quality level");
 % xlabel("Quality Level");
 % ylabel("Numebr of Zeros");
-
-
-
+% grid on;
+% 
+% subplot(1, 2, 2);
+% plot(qua_levels, psnr_vals);
+% title("PSNR vs. Quality level");
+% xlabel("Quality Level");
+% ylabel("PSNR");
+% grid on;
